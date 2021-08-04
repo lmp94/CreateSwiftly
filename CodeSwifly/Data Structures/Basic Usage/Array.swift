7d3d729b8d39b21e8fef058b3d1b86e04d91b47e
Larissa Perara <larissaperara@gmail.com>
2021-08-03 17:45:03 -0700

    formating basic data structures files & clenaing up dictionary

--- CodeSwifly/Data Structures/Basic Usage/Array.swift
+++ CodeSwifly/Data Structures/Basic Usage/Array.swift
@@ -9,75 +9,75 @@
 import Foundation
 
 class ArrayFunctions {
-    
-    // MARK:- Private Class Scoped Values
-    
-    // Shortened forms are preferred
-    private var emptyDoubles: [Double] = []
-    private var emptyInts              = [Int]()
-    private var threeDoubles: [Double] = [0.0, 0.0, 0.0]
   
-    private var shoppingList = ["Eggs", "Milk", "Flour",
-                                "Baking Powder","Chocolate Spread",
-                                "Cheese", "Butter"]
+  // MARK:- Private Class Scoped Values
   
-    // MARK: - Common Functions: append, remove, iterate
-      
-    func append() {
-      // Insertions
-      /// Because arrays increase their allocated capacity
-      /// using an exponential strategy,
-      /// (1) Appending a single element to an array is an O(1) operation
-      /// when averaged over many calls to the method.
-      ///
-      /// When an array has additional capacity and is not sharing its storage with another instance, appending an element is O(1).
-      ///
-      /// When an array needs to reallocate storage before appending or its storage is shared with
-      /// another copy, appending is O(*n*)
-      
-      let threeDoubles = Array(repeating: 0.0, count: 3)
-
-      emptyInts.append(1)
-      
-      var sixDoubles = threeDoubles + emptyDoubles
-      var strings = ["apple"]
-      
-      if strings.isEmpty {
-        strings += ["pear"]
-        sixDoubles += [5.0]
-      }
-      
-      let item = strings[1]
+  // Shortened forms are preferred
+  private var emptyDoubles: [Double] = []
+  private var emptyInts              = [Int]()
+  private var threeDoubles: [Double] = [0.0, 0.0, 0.0]
+  
+  private var shoppingList = ["Eggs", "Milk", "Flour",
+                              "Baking Powder","Chocolate Spread",
+                              "Cheese", "Butter"]
+  
+  // MARK: - Common Functions: append, remove, iterate
+  
+  func append() {
+    // Insertions
+    /// Because arrays increase their allocated capacity
+    /// using an exponential strategy,
+    /// (1) Appending a single element to an array is an O(1) operation
+    /// when averaged over many calls to the method.
+    ///
+    /// When an array has additional capacity and is not sharing its storage with another instance, appending an element is O(1).
+    ///
+    /// When an array needs to reallocate storage before appending or its storage is shared with
+    /// another copy, appending is O(*n*)
+    
+    let threeDoubles = Array(repeating: 0.0, count: 3)
+    
+    emptyInts.append(1)
+    
+    var sixDoubles = threeDoubles + emptyDoubles
+    var strings = ["apple"]
+    
+    if strings.isEmpty {
+      strings += ["pear"]
+      sixDoubles += [5.0]
+    }
+    
+    let item = strings[1]
+    print(item)
+    
+    shoppingList.insert("Maple Syrup", at: 0)
+  }
+  
+  func removal() {
+    /// Complexity: O(1)
+    let lastValue = shoppingList.removeLast()
+    print(lastValue) // Bat
+  }
+  
+  func manipulation() {
+    shoppingList[4...6] = ["Bananas", "Apples"]
+    /* replaces "Chocolate Spread",
+     "Cheese", and "Butter"
+     w/ "Bananas" and "Apples"
+     shopping list now has 4 items */
+  }
+  
+  func iteration() {
+    for item in shoppingList {
       print(item)
-      
-      shoppingList.insert("Maple Syrup", at: 0)
     }
     
-    func removal() {
-      /// Complexity: O(1)
-      let lastValue = shoppingList.removeLast()
-      print(lastValue) // Bat
+    // Enumerated Complexity: O(1)
+    for (index, value) in shoppingList.enumerated() {
+      print("Item \(index + 1): \(value)")
     }
-    
-    func manipulation() {
-      shoppingList[4...6] = ["Bananas", "Apples"]
-      /* replaces "Chocolate Spread",
-                  "Cheese", and "Butter"
-                  w/ "Bananas" and "Apples"
-                  shopping list now has 4 items */
-    }
-    
-    func iteration() {
-      for item in shoppingList {
-        print(item)
-      }
-      
-      // Enumerated Complexity: O(1)
-      for (index, value) in shoppingList.enumerated() {
-        print("Item \(index + 1): \(value)")
-      }
-    }
-    
+  }
+  
 }
 
 // MARK: - Array Manipulation (Filter, Map, Reduce)
@@ -89,7 +89,7 @@ class ArrayFunctions {
 extension ArrayFunctions {
   // Reduces bolierplate code i.e. creating "holder functions" while doing comptation
   // *ANY* collection i.e. dictionary, sets, etc.
-
+  
   // Note: flatmap is deparriciated
   func mapExamples() {
     // non-optional values to transform
@@ -110,12 +110,9 @@ extension ArrayFunctions {
     let values = [1, 4, 5, 6, 7]
     let even = values.filter { $0 % 2 == 0 }
     print(even)
-        
+    
     let names = ["Vivien", "Marlon", "Kim", "Karl"]
     let shortNames = names.filter { $0.count < 5 }
     print(shortNames)
   }
-  
 }
-
-
--- CodeSwifly/Data Structures/Basic Usage/Dictionary.swift
+++ CodeSwifly/Data Structures/Basic Usage/Dictionary.swift
@@ -8,77 +8,99 @@
 
 import Foundation
 
-final class DicionaryFunctions {
+final class DicionaryUsages {
+  // empty initaliziation
+  var namesOfIntegers = [Int: String]()
+  
+  // You do not have to define the type unless
+  var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+  var airports2 = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+  
+  
+  func basics() {
+    namesOfIntegers[16] = "sixteen"
+    namesOfIntegers = [:]
     
-    var namesOfIntegers = [Int: String]() // empty
-    
-    var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
-    var airports2 = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
-    
-    func basic() {
-        
-        // Basic Adding
-        namesOfIntegers[16] = "sixteen"
-        namesOfIntegers = [:]
-        
-        // Empty check
-        if airports.isEmpty {
-            print("The airports dictionary is empty.")
-        } else {
-            print("The airports dictionary is not empty.")
-        }
-        
-        // Updating and Adding
-        
-        airports["LHR"] = "London"
-        
-        if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB") {
-            print("The old value for DUB was \(oldValue).")
-        }
-        
-        // Finding
-        
-        if let airportName = airports["DUB"] {
-            print("The name of the airport is \(airportName).")
-        } else {
-            print("That airport is not in the airports dictionary.")
-        }
-        
-        // Removing
-        
-        airports["APL"] = "Apple International" // "Apple International" is not the real airport for APL, so delete it
-        airports["APL"] = nil // APL has now been removed from the dictionary
-        
-        if let removedValue = airports.removeValue(forKey: "DUB") {
-            print("The removed airport's name is \(removedValue).")
-        } else {
-            print("The airports dictionary does not contain a value for DUB.")
-        }
-        
-        // Iteration
-        for (airportCode, airportName) in airports {
-            print("\(airportCode): \(airportName)")
-        }
-        
-        for airportCode in airports.keys {
-            print("Airport code: \(airportCode)")
-        }
-        
-        for airportName in airports.values {
-            print("Airport name: \(airportName)")
-        }
-        
-        // into Array
-        
-        let airportCodes = [String](airports.keys)
-        // airportCodes is ["LHR", "YYZ"]
-
-        let airportNames = [String](airports.values)
-        // airportNames is ["London Heathrow", "Toronto Pearson"
-        
-        print(airportCodes, airportNames)
+    if airports.isEmpty {
+      print("The airports dictionary is empty.")
+    } else {
+      print("The airports dictionary is not empty.")
     }
-
-
-
+    
+    print("Number of items in `airports`: \(airports.count)")
+    
+    // Finding
+    
+    if let airportName = airports["DUB"] {
+      print("The name of the airport is \(airportName).")
+    } else {
+      print("That airport is not in the airports dictionary.")
+    }
+    
+    // Iteration
+    for (airportCode, airportName) in airports {
+      print("\(airportCode): \(airportName)")
+    }
+    
+    for airportCode in airports.keys {
+      print("Airport code: \(airportCode)")
+    }
+    
+    for airportName in airports.values {
+      print("Airport name: \(airportName)")
+    }
+    
+    // into Array
+    
+    let airportCodes = [String](airports.keys)
+    // airportCodes is ["LHR", "YYZ"]
+    
+    let airportNames = [String](airports.values)
+    // airportNames is ["London Heathrow", "Toronto Pearson"
+    
+    print(airportCodes, airportNames)
+  }
+  
+  // MARK: - Adding Items (TODO)
+  func addingItems() {
+    
+  }
+  
+  
+  // MARK: - Updating Items
+  // we are not using a swift extension becasue we need to
+  // access private class variables
+  func updatingItems() {
+    if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB") {
+      print("The old value for DUB was \(oldValue).")
+    }
+    
+    print(airports)
+    // TODO nil check(?)
+    airports["DUB"] = "Dublin International Airport"
+  }
+  
+  
+  // MARK: - Removing Items
+  // TODO: add airports.remove()
+  
+  func removeItems() {
+    // "Apple International" is not the accurate airport for APL, so delete it
+    airports["APL"] = "Apple International"
+    airports["APL"] = nil // APL has now been removed from the dictionary
+    
+    if let removedValue = airports.removeValue(forKey: "DUB") {
+      print("The removed airport's name is \(removedValue).")
+    } else {
+      print("The airports dictionary does not contain a value for DUB.")
+    }
+  }
+  
+  // MARK:- TODO: compactMapValues, mapValues, merge, sorting, filter
+  // MARK:- TODO: Add Basic Operations:
+  // keys, count,
+  // create a debugDescriptoin
+  // iteration through array of keys (start index etc)
+  
+  
 }
