0b5c9a8d423f06ae603d5543a03a73d2c17a6b3d
Larissa Perara <larissaperara@gmail.com>
2021-08-03 17:31:32 -0700

    clean up array class

--- CodeSwifly/Data Structures/Basic Usage/Array.swift
+++ CodeSwifly/Data Structures/Basic Usage/Array.swift
@@ -14,8 +14,69 @@ class ArrayFunctions {
     
     // Shortened forms are preferred
     private var emptyDoubles: [Double] = []
-    private var emptyInts = [Int]()
+    private var emptyInts              = [Int]()
     private var threeDoubles: [Double] = [0.0, 0.0, 0.0]
+  
+    private var shoppingList = ["Eggs", "Milk", "Flour",
+                                "Baking Powder","Chocolate Spread",
+                                "Cheese", "Butter"]
+  
+    // MARK: - Common Functions: append, remove, iterate
+      
+    func append() {
+      // Insertions
+      /// Because arrays increase their allocated capacity
+      /// using an exponential strategy,
+      /// (1) Appending a single element to an array is an O(1) operation
+      /// when averaged over many calls to the method.
+      ///
+      /// When an array has additional capacity and is not sharing its storage with another instance, appending an element is O(1).
+      ///
+      /// When an array needs to reallocate storage before appending or its storage is shared with
+      /// another copy, appending is O(*n*)
+      
+      let threeDoubles = Array(repeating: 0.0, count: 3)
+
+      emptyInts.append(1)
+      
+      var sixDoubles = threeDoubles + emptyDoubles
+      var strings = ["apple"]
+      
+      if strings.isEmpty {
+        strings += ["pear"]
+        sixDoubles += [5.0]
+      }
+      
+      let item = strings[1]
+      print(item)
+      
+      shoppingList.insert("Maple Syrup", at: 0)
+    }
+    
+    func removal() {
+      /// Complexity: O(1)
+      let lastValue = shoppingList.removeLast()
+      print(lastValue) // Bat
+    }
+    
+    func manipulation() {
+      shoppingList[4...6] = ["Bananas", "Apples"]
+      /* replaces "Chocolate Spread",
+                  "Cheese", and "Butter"
+                  w/ "Bananas" and "Apples"
+                  shopping list now has 4 items */
+    }
+    
+    func iteration() {
+      for item in shoppingList {
+        print(item)
+      }
+      
+      // Enumerated Complexity: O(1)
+      for (index, value) in shoppingList.enumerated() {
+        print("Item \(index + 1): \(value)")
+      }
+    }
     
 }
 
@@ -57,75 +118,4 @@ extension ArrayFunctions {
   
 }
 
-// MARK: - Common Functions: append, remove, iterate
 
-extension ArrayFunctions {
-    
-    func append() {
-        let threeDoubles = Array(repeating: 0.0, count: 3)
-        
-        // Insertions
-        // Because arrays increase their allocated capacity using an exponential
-         /// strategy,
-         /// (1) Appending a single element to an array is an O(1) operation
-         /// when averaged over many calls to the method.
-         ///
-         /// When an array has additional capacity and is not sharing its storage with another
-         /// instance, appending an element is O(1).
-         ///
-         /// When an array needs to reallocate storage before appending or its storage is shared with
-         /// another copy, appending is O(*n*)
-        emptyInts.append(1)
-        
-        var sixDoubles = threeDoubles + emptyDoubles
-        var strings = ["apple"]
-        
-        if strings.isEmpty {
-            strings += ["pear"]
-            sixDoubles += [5.0]
-        }
-        
-        let item = strings[1]
-        print(item)
-        
-        shoppingList.insert("Maple Syrup", at: 0)
-    }
-    
-    func removal() {
-        var toys = ["Ball", "Bat" ]
-        
-        /// Complexity: O(1)
-        let lastValue = shoppingList.removeLast()
-        print(lastValue) // Bat
-    }
-    
-    func manipulaation(_ array: Array) {
-        var shoppingList = ["Eggs", "Milk", "Flour",
-                            "Baking Powder", "Chocolate Spread", "Cheese",
-                            "Butter"]
-        
-        shoppingList[4...6] = ["Bananas", "Apples"]
-        /* replaces "Chocolate Spread",
-                    "Cheese", and
-                    "Butter"
-         with "Bananas" and "Apples"
-        shopping list now has 4 items */
-    }
-    
-    func iteration() {
-        let shoppingList = ["Eggs", "Milk", "Flour", "Baking Powder", "Chocolate Spread", "Cheese", "Butter"]
-        
-        for item in shoppingList {
-            print(item)
-        }
-
-        
-        for (index, value) in shoppingList.enumerated() { // Enumerated Complexity: O(1)
-            print("Item \(index + 1): \(value)")
-        }
-    }
-    
-    func transformation() {
-        let startingList = ["Eggs", "Milk", "Flour", "Baking Powder", "Chocolate Spread", "Cheese", "Butter"]
-    }
-}
